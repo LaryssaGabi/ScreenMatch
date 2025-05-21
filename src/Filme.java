641d570260765cff190483e8f44ef01b8802699b
LaryssaGabi <lary.gabriela85@gmail.com>
2025-05-20 22:35:40 -0300

    metodos

--- src/Filme.java
+++ src/Filme.java
@@ -2,7 +2,22 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double avaliacao;
+    double somaDasValiacoes;
     int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    void exibirFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Duração do filme: " + duracaoEmMinutos + "min");
+    }
+
+    void avalia(double nota) {
+        somaDasValiacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia(){
+        return somaDasValiacoes / totalDeAvaliacoes;
+    }
 }
