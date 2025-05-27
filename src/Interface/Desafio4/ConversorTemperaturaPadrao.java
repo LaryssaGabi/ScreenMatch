77eddd66625e1ab26d67b420874e2b9a11016ba9
LaryssaGabi <lary.gabriela85@gmail.com>
2025-05-27 19:50:27 -0300

    finalizando o desafio

--- src/Interface/Desafio4/ConversorTemperaturaPadrao.java
+++ src/Interface/Desafio4/ConversorTemperaturaPadrao.java
@@ -1,5 +1,13 @@
 package Interface.Desafio4;
 
-public class ConversorTemperaturaPadrao {
+public class ConversorTemperaturaPadrao implements ConversorTemperatura{
+    @Override
+    public double celsiusParaFahrenheit(double temperaturaCelsius) {
+        return (temperaturaCelsius * 9 / 5) + 32;
+    }
 
+    @Override
+    public double fahrenheitParaCelsius(double temperaturaFahrenheit) {
+        return (temperaturaFahrenheit - 32) * 5 / 9;
+    }
 }
